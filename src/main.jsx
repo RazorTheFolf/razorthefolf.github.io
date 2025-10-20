558ea95c418806a32ad28706583bf3f9a7f58113
Razor <72689902+RazorTheFolf@users.noreply.github.com>
2025-10-20 15:39:00 +0200

    Fix GitHub link in console log message

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ console.log("Hewoo :3");
 console.log("I'm glad you're here :D");
 console.log("So you're a developer too ?");
 console.log(
-  "You can check the code here: https://github.com/RazorTheWolf/razorthewolf.github.io"
+  "You can check the code here: https://github.com/RazorTheFolf/razorthefolf.github.io"
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
